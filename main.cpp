911305311e03714158ad69e7afa8ebc5cd518819
tritone4 <shingokitada@gmail.com>
2019-08-04 23:04:56 +0900

    iterator exercise

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
-#include <iostream>
-// #include <matplotlib-cpp/matplotlibcpp.h>
 #include "all.h"
 
-// using namespace std;
-// namespace plt = matplotlibcpp;
-
 int main(){
-    int n = 1000;
-    std::vector<double> x(n), y(n);
- 
-    for(int i=0; i<n; ++i) {
-        x[i] = 2 * M_PI * i / n;
-        y[i] = sin(x[i]);
+
+//    std::string line;
+//
+//    while (getline(std::cin, line)){
+//        std::cout << "(" << line << ")" << std::endl;
+//    }
+
+// iterator
+    class eki {
+        std::string ecode;
+        std::string ename;
+    };
+
+    std::vector<eki> ekiv = {};
+    std::vector<int> v = {111111,222222,333333,444444,555555};
+
+    auto i = std::begin(v);
+    eki e;
+    
+
+    for ( int val : v ){
+        e = new eki;
+        e.ecode = std::to_string(val);
+        e.ename = e.ecode + "station";
+
+        eki.push_back(e);
     }
-    // matplotlibcpp::plot(x, y);
-    // matplotlibcpp::show();
+
+    for ( eki eval : ekiv ){
+        std::cout << "ecode=" << eval.ecode << " / ename=" << eval.ename << std::endl;
+    }
 }
